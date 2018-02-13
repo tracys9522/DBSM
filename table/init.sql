bf5eae2976200a451b097fe698c0e55f5bfd95b0
gsun <gsun@scu.edu>
2018-02-13 12:46:20 -0800

    UPDATE

--- table/init.sql
+++ table/init.sql
@@ -1,3 +1,24 @@
 create table customer(
-  
+  custid varchar(5) primary key,
+  name varchar(10),
+  phoneno int
+);
+
+create table servicecontract
+(
+  contracteid varchar(5) primary key,
+  machineid varchar(5),
+  startdate date,
+  enddate date,
+  custinfo varchar(100)
+);
+
+create table repairjob
+(
+  machienid varchar(5),
+  servicecontractid varchar(5),
+  arrivaltime datetime,
+  ownerinfo varchar(100),
+  status varchar(15) not null,
+  check (status in('UNDER_REPAIR','READY','DONE'))
 );
