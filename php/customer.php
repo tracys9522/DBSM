1a6b1eb18641f764d4f8c73a97368ab238910864
gsun <gsun@scu.edu>
2018-02-22 17:18:27 -0800

    custphp without custid trigger

--- php/customer.php
+++ php/customer.php
@@ -1,46 +1,39 @@
 <?php
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-     // collect input data
-	// Get the Name
+
    $name = $_POST['name'];
-	 // Get the price
-	 $phoneno = $_POST['phoneno'];
+   $phone = $_POST['phoneno'];
 
-     // Validate input
-    if (!empty($name)){
-		$name = prepareInput($name);
-     }
-	  if (!empty($phoneno)){
-		$phoneno = prepareInput($phoneno);
-     }
-	// Call the function to insert name, calories,price
-	// into FoodItem table
+   // Validate input
+   if (!empty($name)){
+	 $name = prepareInput($name);
+   }
 
-	insertFoodInputIntoDB($name,$phoneno);
+   if (!empty($phoneno)){
+	 $phoneno = prepareInput($phoneno);
+   }
 
+   insertCustomerIntoDB($name,$phoneno);
 }
+
 function prepareInput($inputData){
 	$inputData = trim($inputData);
-  	$inputData  = htmlspecialchars($inputData);
-  	return $inputData;
+  $inputData  = htmlspecialchars($inputData);
+  return $inputData;
 }
 
-
-
-function insertFoodInputIntoDB($name,$phoneno){
+function insertCustomerIntoDB($name,$phoneno){
 
 	//connect to your database
-	$conn=oci_connect('username','passwd', 'localhost/XE');
+	$conn=oci_connect('tsun','pass', '//dbserver.engr.scu.edu/db11g');
 	if(!$conn) {
 	     print "<br> connection failed:";
-           exit;
+       exit;
 	}
-	// convert the string to a number
-	$calories = intval($calories);
-	$price = (float) $price;
 
-$query = oci_parse($conn, "Insert Into FoodItem(itemname,calories,price) values(:name,:calories,:price)");
+  $phoneno = (char) $phoneno;
+
+  $query = oci_parse($conn, "Insert Into customer(name,phoneno) values(:name,:phoneno)");
 
 	// Program variables are bound to SQL statement
 
