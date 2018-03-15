4a366621e41df33d62bfc80bae116bb90577b541
Tracy Sun <gsun@scu.edu>
2018-03-14 17:43:22 -0700

    Create cStatus.php

---
+++ submit/front/cStatus.php
@@ -0,0 +1,26 @@
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+  $machineid = $_POST['machineid'];
+  showStatus($machineid);
+}
+function showStatus($machineid){
+	//connect to your database
+	$conn=oci_connect('tsun','krit922954', '//dbserver.engr.scu.edu/db11g');
+	if(!$conn) {
+	     print "<br> connection failed:";
+       exit;
+	}
+       $query = oci_parse($conn, "select * from repairjob");
+       oci_execute($query);
+       while(($row = oci_fetch_array($query, OCI_BOTH)) != false){
+	  if($row[0] == $machineid){
+	      echo "Machine ID: " .$row[0]. "";
+	      echo "<p></p>";
+	      echo "Status: " .$row[5]. "";
+	  }
+	  
+      }
+OCILogoff($conn);
+}
+?>
