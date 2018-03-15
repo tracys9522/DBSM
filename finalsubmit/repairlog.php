07981b68d12d32cbc1bb34781ae402a88a2be21e
Tracy Sun <gsun@scu.edu>
2018-03-15 14:43:35 -0700

    Add files via upload

---
+++ finalsubmit/repairlog.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+   </head>
+  <body>
+
+<?php
+
+showlog();
+function showlog(){
+
+	//connect to your database
+	$conn=oci_connect('tsun','krit922954', '//dbserver.engr.scu.edu/db11g');
+	if(!$conn) {
+	     print "<br> connection failed:";
+       exit;
+	}
+
+  	$query = oci_parse($conn,"select * from repairLog");
+
+	// Execute the query
+    oci_execute($query);
+	while($row = oci_fetch_array($query)){
+		echo 'machineid: ',$row[0],'</br>';
+		echo 'service contract id: ',$row[1],'</br>';
+		echo 'arrival date: ',$row[2],'</br>';
+		echo 'customer id: ',$row[3],'</br>';
+		echo 'covered? ',$row[4],'</br>';
+		echo '</br></br>';
+	}
+	
+	OCILogoff($conn);
+}
+?>
+
+  <p>
+    <input type ="button" value="Go Back" onclick="window.location.href = 'employee.html'">
+  </p>
+   </body>
+</html>
